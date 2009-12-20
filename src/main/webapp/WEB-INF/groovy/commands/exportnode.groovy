3a669c3f789739dbd88e999f58b6d96038722ef5
julien.viet <julien.viet@118f38d8-d83b-11de-afbc-5bc42a3db48a>
2009-12-20 22:04:33 +0000

    converted exportnode to command

--- src/main/webapp/WEB-INF/groovy/commands/exportnode.groovy
+++ src/main/webapp/WEB-INF/groovy/commands/exportnode.groovy
@@ -3,51 +3,62 @@ import java.io.ByteArrayOutputStream;
 import java.io.ByteArrayInputStream;
 import java.util.Calendar;
 import org.crsh.console.ConsoleBuilder;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.Argument;
 
 /*
  * Exports a node to a JCR file.
  */
-{ String srcPath, String dstPath ->
-  assertConnected();
+class exportnode extends org.crsh.shell.ClassCommand {
 
-  // Source node to export
-  def srcNode = findNodeByPath(srcPath);
+  @Argument(required=true,index=0,usage="Source path")
+  def String srcPath;
 
-  //
-  def session = srcNode.session;
+  @Argument(required=true,index=1,usage="Destination path")
+  def String dstPath;
 
-  // Destination parent
-  int pos = dstPath.lastIndexOf('/');
-  if (pos == -1)
-    throw new ScriptException("The destination must be absolute");
-  def dstParenNodet;
-  def dstName;
-  if (pos == 0) {
-    dstParentNode = findNodeByPath("/");
-    dstName = dstPath.substring(1);
-  } else {
-    dstParentNode = findNodeByPath(dstPath.substring(0, pos));
-    dstName = dstPath.substring(pos + 1);
+  public Object execute() throws ScriptException {
+    assertConnected();
+
+    // Source node to export
+    def srcNode = findNodeByPath(srcPath);
+
+    //
+    def session = srcNode.session;
+
+    // Destination parent
+    int pos = dstPath.lastIndexOf('/');
+    if (pos == -1)
+      throw new ScriptException("The destination must be absolute");
+    def dstParenNodet;
+    def dstName;
+    if (pos == 0) {
+      dstParentNode = findNodeByPath("/");
+      dstName = dstPath.substring(1);
+    } else {
+      dstParentNode = findNodeByPath(dstPath.substring(0, pos));
+      dstName = dstPath.substring(pos + 1);
+    }
+
+    //
+    if (dstParentNode[dstName] != null) {
+      throw new ScriptException("Destination path already exist");
+    }
+
+    // Export
+    def baos = new ByteArrayOutputStream();
+    srcNode.session.exportSystemView(srcNode.path, baos, false, false);
+    baos.close();
+
+    // Create nt file / nt resource
+    def fileNode = dstParentNode.addNode(dstName, "nt:file");
+    def res = fileNode.addNode("jcr:content", "nt:resource");
+    def bais = new ByteArrayInputStream(baos.toByteArray());
+    res.setProperty("jcr:mimeType", "application/xml");
+    res.setProperty("jcr:data", bais);
+    res.setProperty("jcr:lastModified", Calendar.getInstance());
+
+    //
+    return "The node has been exported";
   }
-
-  //
-  if (dstParentNode[dstName] != null) {
-    throw new ScriptException("Destination path already exist");
-  }
-
-  // Export
-  def baos = new ByteArrayOutputStream();
-  srcNode.session.exportSystemView(srcNode.path, baos, false, false);
-  baos.close();
-
-  // Create nt file / nt resource
-  def fileNode = dstParentNode.addNode(dstName, "nt:file");
-  def res = fileNode.addNode("jcr:content", "nt:resource");
-  def bais = new ByteArrayInputStream(baos.toByteArray());
-  res.setProperty("jcr:mimeType", "application/xml");
-  res.setProperty("jcr:data", bais);
-  res.setProperty("jcr:lastModified", Calendar.getInstance());
-
-  //
-  return "The node has been exported";
 }
