101256f22e8ad31c6a091a2992be3c98d9847a37
julien.viet <julien.viet@118f38d8-d83b-11de-afbc-5bc42a3db48a>
2009-12-22 13:59:38 +0000

    improved default query limit

--- src/main/webapp/WEB-INF/groovy/commands/select.groovy
+++ src/main/webapp/WEB-INF/groovy/commands/select.groovy
@@ -4,17 +4,35 @@ import org.crsh.display.DisplayBuilder;
 import org.kohsuke.args4j.Option;
 import org.crsh.shell.Description;
 
-@Description("Executes a query with the SQL dialect")
+@Description("Executes a query with the SQL dialect, by default results are limited to 5 ")
 public class select extends org.crsh.shell.AnyArgumentClassCommand {
 
   @Option(name="-o",aliases=["--offset"],usage="The result offset")
-  def Integer offset;
+  def Integer offset = 0;
 
   @Option(name="-l",aliases=["--limit"],usage="The result limit")
-  def Integer limit;
+  def Integer limit = 5;
+
+  @Option(name="-a",aliases=["--all"],usage="Ignore the limit argument")
+  def Boolean all = false;
 
   public Object execute() throws ScriptException {
     assertConnected();
+
+    //
+    if (offset < 0) {
+      return "No negative offset accepted";
+    }
+
+    //
+    if (limit < 0) {
+      return "No negative limit accepted";
+    }
+
+    //
+    if (all) {
+      limit = null;
+    }
 
     //
     def queryMgr = session.workspace.queryManager;
@@ -34,7 +52,8 @@ public class select extends org.crsh.shell.AnyArgumentClassCommand {
 
     //
     def nodes = result.nodes;
-    if (offset != null) {
+    def total = nodes.size;
+    if (offset > 0) {
       nodes.skip(offset);
     }
 
@@ -42,7 +61,7 @@ public class select extends org.crsh.shell.AnyArgumentClassCommand {
     def builder = new DisplayBuilder();
 
     //
-    builder.node {
+    builder.node("The query matched " + total + " nodes") {
       def index = 0;
       while (nodes.hasNext()) {
         def n = nodes.next();
