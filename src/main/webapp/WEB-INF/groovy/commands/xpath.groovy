b2c9cea572d153baa5b0448b677f4d37fd809fa6
julien.viet <julien.viet@118f38d8-d83b-11de-afbc-5bc42a3db48a>
2010-02-15 23:37:44 +0000

    Issue 3: xpath contribution

---
+++ src/main/webapp/WEB-INF/groovy/commands/xpath.groovy
@@ -0,0 +1,77 @@
+import org.kohsuke.args4j.Argument;
+import javax.jcr.query.Query;
+import org.crsh.display.DisplayBuilder;
+import org.kohsuke.args4j.Option;
+import org.crsh.shell.Description;
+
+@Description("Executes a query with the XPATH dialect, by default results are limited to 5 ")
+public class select extends org.crsh.shell.ClassCommand {
+
+  @Option(name="-o",aliases=["--offset"],usage="The result offset")
+  def Integer offset = 0;
+
+  @Option(name="-l",aliases=["--limit"],usage="The result limit")
+  def Integer limit = 5;
+
+  @Option(name="-a",aliases=["--all"],usage="Ignore the limit argument")
+  def Boolean all = false;
+
+  @Argument(required=true,index=0,usage="The xpath query to execute")
+  def String xpath;
+
+  public Object execute() throws ScriptException {
+    assertConnected();
+
+    //
+    if (offset < 0) {
+      return "No negative offset accepted";
+    }
+
+    //
+    if (limit < 0) {
+      return "No negative limit accepted";
+    }
+
+    //
+    if (all) {
+      limit = null;
+    }
+
+    //
+    def queryMgr = session.workspace.queryManager;
+
+    //
+    def query = queryMgr.createQuery(xpath, Query.XPATH);
+
+    //
+    def result = query.execute();
+
+    // Column we will display
+    def columnNames = [];
+
+    //
+    def nodes = result.nodes;
+    def total = nodes.size;
+    if (offset > 0) {
+      nodes.skip(offset);
+    }
+
+    //
+    def builder = new DisplayBuilder();
+
+    //
+    builder.node("The query matched " + total + " nodes") {
+      def index = 0;
+      while (nodes.hasNext()) {
+        def n = nodes.next();
+        if (limit != null && index >= limit)
+          break;
+        formatNode(builder, n, 0, 1);
+        index++;
+      }
+    }
+
+    //
+    return builder;
+  }
+}
