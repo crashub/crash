8ed9bee8ddd9b2c3fd138b68b3defdffeb20e05d
julien.viet <julien.viet@118f38d8-d83b-11de-afbc-5bc42a3db48a>
2010-02-16 14:02:07 +0000

    forgot to add invoke command

---
+++ src/test/resources/commands/invoke.groovy
@@ -0,0 +1,21 @@
+import org.crsh.shell.Description;
+import org.kohsuke.args4j.Argument;
+import org.crsh.shell.Description;
+import org.crsh.shell.ScriptException;
+import java.lang.reflect.Method;
+
+@Description("Invoke a static method")
+public class invoke extends org.crsh.shell.ClassCommand {
+
+  @Argument(required=false,index=0,usage="The fqn of the class to resolve")
+  def String className;
+
+  @Argument(required=false,index=1,usage="The method to invoke")
+  def String methodName;
+
+  public Object execute() throws ScriptException {
+    Class<?> clazz = Thread.currentThread().getContextClassLoader().loadClass(className);
+    Method m = clazz.getMethod(methodName);
+    m.invoke(null);
+  }
+}
