f9298f3c7ad7d5171cfa45d00c15a745db7a4359
julien.viet <julien.viet@118f38d8-d83b-11de-afbc-5bc42a3db48a>
2010-05-24 22:15:10 +0000

    minor

--- core/src/main/java/org/crsh/shell/Shell.java
+++ core/src/main/java/org/crsh/shell/Shell.java
@@ -19,8 +19,6 @@
 
 package org.crsh.shell;
 
-import java.util.concurrent.Callable;
-
 /**
  * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
  * @version $Revision$
--- core/src/main/java/org/crsh/shell/impl/CRaSH.java
+++ core/src/main/java/org/crsh/shell/impl/CRaSH.java
@@ -34,7 +34,6 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
