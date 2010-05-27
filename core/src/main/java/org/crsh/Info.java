87eef13a09730133df2940c877f301d5442a2bc8
julien.viet <julien.viet@118f38d8-d83b-11de-afbc-5bc42a3db48a>
2010-05-27 13:28:04 +0000

    beta8

--- core/src/main/java/org/crsh/Info.java
+++ core/src/main/java/org/crsh/Info.java
@@ -25,7 +25,7 @@ package org.crsh;
 public class Info {
 
   /** . */
-  private static final String VERSION = "1.0.0-beta4";
+  private static final String VERSION = "1.0.0-beta8";
 
   public static String getVersion() {
     return VERSION;
