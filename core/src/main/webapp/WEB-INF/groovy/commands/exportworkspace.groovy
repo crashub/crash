e2d876ab05ea2b342f08d4ebd5ac4ad4dfac3905
julien.viet <julien.viet@118f38d8-d83b-11de-afbc-5bc42a3db48a>
2010-10-14 13:11:40 +0000

    export/import

---
+++ core/src/main/webapp/WEB-INF/groovy/commands/exportworkspace.groovy
@@ -0,0 +1,162 @@
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Calendar;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.Argument;
+import org.crsh.command.ScriptException;
+import org.crsh.command.Description;
+import org.crsh.jcr.JCR;
+import javax.jcr.Node;
+import javax.jcr.SimpleCredentials;
+import javax.jcr.Property;
+import javax.jcr.PropertyType;
+import javax.jcr.Value;
+import javax.jcr.ValueFormatException;
+
+@Description("Export a workspace on the file system")
+class exportworkspace extends org.crsh.command.ClassCommand {
+
+  @Option(required=false,name="-u",aliases=["--username"],usage="user name")
+  def String userName;
+
+  @Option(name="-p",required=false,aliases=["--password"],usage="password")
+  def String password;
+
+  @Option(name="-c",aliases=["--container"],usage="portal container name (eXo portal specific)")
+  def String containerName;
+
+  @Option(name="-w",aliases=["--workspace"],usage="the workspace name")
+  def String workspaceName;
+
+  @Argument(required=true,index=0,usage="The export file name")
+  def String exportFileName;
+
+  public Object execute() throws ScriptException {
+
+    //
+    File file = new File(exportFileName);
+    if (file.exists()) {
+      throw new ScriptException("File $file.absolutePath already exist");
+    }
+
+    //
+    def exportSession;
+    boolean close;
+    if (session != null) {
+      exportSession = session;
+      close = false;
+    } else {
+      // Obtain the repository
+      def properties = containerName == null ? [:] : ["exo.container.name":containerName];
+      def repo = JCR.getRepository(properties);
+
+      //
+      if (userName == null) {
+        throw new ScriptException("No username provided");
+      }
+      if (password == null) {
+        throw new ScriptException("No password provided");
+      }
+      if (workspaceName == null) {
+        throw new ScriptException("No workspace name provided");
+      }
+
+      // Get credentials
+      def credentials = new SimpleCredentials(userName, password.toCharArray());
+
+      // Get session
+      exportSession = repo.login(credentials, workspaceName);
+      close = true;
+    }
+    try {
+      exportNode(exportSession.rootNode, file);
+      return "Exported workspace $exportSession.workspace.name to $file.absolutePath";
+    } finally {
+      if (close)
+        exportSession.logout();
+    }
+  }
+
+  private void exportNode(Node node, File file) {
+    def OutputStream out = new DataOutputStream(new FileOutputStream(file));
+    try {
+      exportNode(node, [] as Set, out);
+    } finally {
+      out.close();
+    }
+  }
+
+  private void exportNode(Node node, Set exportedNodes, DataOutputStream out) {
+    if (!exportedNodes.contains(node.path)) {
+      exportedNodes.add(node.path);
+
+      // True if the node should be created
+      out.writeBoolean(node.path != "/" && !node.definition.autoCreated);
+
+      //
+      out.writeUTF(node.path);
+      out.writeUTF(node.name);
+      out.writeUTF(node.primaryNodeType.name);
+      out.writeInt(node.mixinNodeTypes.length);
+      node.mixinNodeTypes.each { mixinNodeType ->
+        out.writeUTF(mixinNodeType.name);
+      }
+
+      //
+      node.each { child ->
+        // 0 means child node
+        out.writeInt(0);
+        exportNode(child, exportedNodes, out);
+      }
+      node.eachProperty { property ->
+        exportProperty(property, exportedNodes, out);
+      }
+
+      // 2 means we are done with children
+      out.writeInt(2);
+    }
+  }
+
+  private void exportProperty(Property property, Set exportedNodes, DataOutputStream out) {
+    if (property == null) {
+      throw new NullPointerException("No null property accepted");
+    }
+    if (!property.definition.protected) {
+
+      // 1 means child property
+      out.writeInt(1);
+      out.writeUTF(property.name);
+
+      //
+      try {
+        def value = property.value;
+        // True means single valued property
+        out.writeBoolean(true);
+        exportValue(value, exportedNodes, out);
+      } catch (ValueFormatException e) {
+        // False means multi valued property
+        out.writeBoolean(false);
+        out.writeInt(property.values.length);
+        property.values.each { value ->
+          exportValue(value, exportedNodes, out);
+        };
+      }
+    }
+  }
+
+  private void exportValue(Value value, Set exportedNodes, DataOutputStream out) {
+    if (value == null) {
+      throw new NullPointerException("No null value accepted");
+    }
+    out.writeInt(value.type);
+    if (value.type == PropertyType.REFERENCE) {
+      out.writeUTF(value.node.path);
+      exportNode(value.node, exportedNodes, out);
+    } else {
+      out.writeUTF(value.string);
+    }
+  }
+}
---
+++ core/src/main/webapp/WEB-INF/groovy/commands/importworkspace.groovy
@@ -0,0 +1,188 @@
+import java.io.InputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Calendar;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.Argument;
+import org.crsh.command.ScriptException;
+import org.crsh.command.Description;
+import org.crsh.jcr.JCR;
+import javax.jcr.Node;
+import javax.jcr.SimpleCredentials;
+import javax.jcr.Property;
+import javax.jcr.PropertyType;
+import javax.jcr.Value;
+import javax.jcr.ValueFormatException;
+
+@Description("Import a workspace from the file system")
+class workspaceimport extends org.crsh.command.ClassCommand {
+
+  @Option(required=false,name="-u",aliases=["--username"],usage="user name")
+  def String userName;
+
+  @Option(name="-p",required=false,aliases=["--password"],usage="password")
+  def String password;
+
+  @Option(name="-c",aliases=["--container"],usage="portal container name (eXo portal specific)")
+  def String containerName;
+
+  @Option(name="-w",aliases=["--workspace"],usage="the workspace name")
+  def String workspaceName;
+
+  @Argument(required=true,index=0,usage="The export file name")
+  def String exportFileName;
+
+  public Object execute() throws ScriptException {
+
+    //
+    File file = new File(exportFileName);
+    if (!file.exists()) {
+      throw new ScriptException("File $file.absolutePath does not exist");
+    }
+
+    //
+    def importSession;
+    boolean close;
+    if (session != null) {
+      importSession = session;
+      close = false;
+    } else {
+      // Obtain the repository
+      def properties = containerName == null ? [:] : ["exo.container.name":containerName];
+      def repo = JCR.getRepository(properties);
+
+      //
+      if (userName == null) {
+        throw new ScriptException("No username provided");
+      }
+      if (password == null) {
+        throw new ScriptException("No password provided");
+      }
+      if (workspaceName == null) {
+        throw new ScriptException("No workspace name provided");
+      }
+
+      // Get credentials
+      def credentials = new SimpleCredentials(userName, password.toCharArray());
+
+      // Get session
+      importSession = repo.login(credentials, workspaceName);
+      close = true;
+    }
+    try {
+      importRoot(importSession.rootNode, file);
+      return "Imported $file.absolutePath in workspace $importSession.workspace.name";
+    } finally {
+      if (close)
+        importSession.logout();
+    }
+  }
+
+  private void importRoot(Node node, File file) {
+    def InputStream input = new FileInputStream(file);
+    input = new DataInputStream(input);
+    try {
+
+      // Skip create
+      input.readBoolean();
+      // Skip path
+      input.readUTF();
+      // Skip name
+      input.readUTF();
+      // Skip primaryNodeType
+      input.readUTF();
+
+      // Import state
+      def importedNodes = [:];
+      importedNodes["/"] = node;
+      importNodeState(node, importedNodes, input);
+    } finally {
+      input.close();
+    }
+  }
+
+  private Node importNode(Node parent, Map importedNodes, DataInputStream input) {
+
+    // Should we created it or not
+    def create = input.readBoolean();
+    // Get path
+    def path = input.readUTF();
+    // Get name
+    def name = input.readUTF();
+    // Get primaryNodeType
+    def primaryNodeType = input.readUTF();
+
+    // Create node
+    def child;
+    if (create) {
+      child = parent.addNode(name, primaryNodeType);
+      System.out.println("added node $child.path");
+    }
+    else
+      child = parent.getNode(name);
+
+    // Add to map
+    importedNodes[path] = child;
+
+    // Import state
+    importNodeState(child, importedNodes, input);
+
+    //
+    return node;
+  }
+
+  private void importNodeState(Node node, Map importedNodes, DataInputStream input) {
+    // Add mixins
+    def mixinCount = input.readInt();
+    while (mixinCount-- > 0) {
+      def mixinName = input.readUTF();
+      node.addMixin(mixinName);
+    }
+
+    // Add children
+    out:
+    while (true) {
+      switch (input.readInt()) {
+        case 0:
+          importNode(node, importedNodes, input);
+          break;
+        case 1:
+          def propertyName = input.readUTF();
+          def single = input.readBoolean();
+          if (single) {
+            def value = importPropertyValue(node, importedNodes, input);
+            node.setProperty(propertyName, value);
+          } else {
+            int propertyCount = input.readInt();
+            def values = [];
+            while (propertyCount-- > 0) {
+              def value = importPropertyValue(node, importedNodes, input);
+              values.add(value);
+            }
+            node.setProperty(propertyName, values.toArray(new Value[values.size()]));
+          }
+          break;
+        case 2:
+          break out;
+        default:
+          throw new AssertionError("should not be here");
+      }
+    }
+  }
+
+  private Value importPropertyValue(Node node, Map importedNodes, DataInputStream input) {
+    def type = input.readInt();
+    if (type == PropertyType.REFERENCE) {
+      def path = input.readUTF();
+      def referencedNode = importedNodes[path];
+      if (referencedNode == null) {
+        referencedNode = importNode(node, importedNodes, input);
+      }
+      return node.session.valueFactory.createValue(referencedNode);
+    } else {
+      def stringValue = input.readUTF();
+      return node.session.valueFactory.createValue(stringValue, type);
+    }
+  }
+}
