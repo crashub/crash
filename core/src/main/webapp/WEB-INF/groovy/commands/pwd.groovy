5d13cdec372137182cf756c5cf59910c757ea538
julien.viet <julien.viet@118f38d8-d83b-11de-afbc-5bc42a3db48a>
2010-11-09 06:39:56 +0000

    minor javadoc

--- core/src/main/webapp/WEB-INF/groovy/commands/pwd.groovy
+++ core/src/main/webapp/WEB-INF/groovy/commands/pwd.groovy
@@ -1,7 +1,7 @@
 import org.crsh.command.Description;
 import org.crsh.command.CommandContext;
 
-@Description("Print the current path")
+@Description("Print the current path. The current node is produced by this command")
 public class pwd extends org.crsh.command.BaseCommand<Void, Node> {
 
   public void execute(CommandContext<Node, Void> context) throws ScriptException {
