9433942ba0376191c768176e56c0dcee3db7a8f3
julien.viet <julien.viet@118f38d8-d83b-11de-afbc-5bc42a3db48a>
2010-12-14 23:50:35 +0000

    missing those classes

---
+++ shell/core/src/main/java/org/crsh/util/Strings.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.crsh.util;
+
+import java.util.Arrays;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+public class Strings {
+
+  /**
+   * @see #findLongestCommonPrefix(Iterable)
+   */
+  public static String findLongestCommonPrefix(CharSequence... seqs) {
+    return findLongestCommonPrefix(Arrays.asList(seqs));
+  }
+
+  /**
+   * Find the longest possible common prefix of the provided char sequence.
+   *
+   * @param seqs the sequences
+   * @return the longest possible prefix
+   */
+  public static String findLongestCommonPrefix(Iterable<? extends CharSequence> seqs) {
+    String common = "";
+    out:
+    while (true) {
+      String candidate = null;
+      for (CharSequence s : seqs) {
+        if (common.length() + 1 > s.length()) {
+          break out;
+        } else {
+          if (candidate == null) {
+            candidate = s.subSequence(0, common.length() + 1).toString();
+          } else if (s.subSequence(0, common.length() + 1).toString().equals(candidate)) {
+            // Ok it is a prefix
+          } else {
+            break out;
+          }
+        }
+      }
+      if (candidate == null) {
+        break;
+      } else {
+        common = candidate;
+      }
+    }
+    return common;
+  }
+}
