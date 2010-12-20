e12b8b1ec760d39496cd9462ed02c374c19c48e8
julien.viet <julien.viet@118f38d8-d83b-11de-afbc-5bc42a3db48a>
2010-12-20 12:54:31 +0000

    start to add processor

---
+++ shell/core/src/main/java/org/crsh/cmdline/processor/CmdLineProcessor.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.crsh.cmdline.processor;
+
+import org.crsh.cmdline.CommandDescriptor;
+import org.crsh.cmdline.Multiplicity;
+import org.crsh.cmdline.ParameterBinding;
+import org.crsh.cmdline.ParameterDescriptor;
+import org.crsh.cmdline.analyzer.Analyzer;
+import org.crsh.cmdline.analyzer.Match;
+import org.crsh.cmdline.analyzer.MatchIterator;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+public abstract class CmdLineProcessor<T, B extends ParameterBinding> {
+
+  /** . */
+  protected final CommandDescriptor<T, B> descriptor;
+
+  /** . */
+  protected final Analyzer<T, B> analyzer;
+
+  public CmdLineProcessor(CommandDescriptor<T, B> descriptor) {
+    this.descriptor = descriptor;
+    this.analyzer = new Analyzer<T, B>(descriptor);
+  }
+
+  public static class Clazz<T> extends CmdLineProcessor<T, ParameterBinding.ClassField> {
+
+    public Clazz(CommandDescriptor<T, ParameterBinding.ClassField> tClassFieldCommandDescriptor) {
+      super(tClassFieldCommandDescriptor);
+    }
+
+    public void process(T object, String s) throws CmdLineException {
+
+      //
+      Set<ParameterDescriptor<?>> foo = new HashSet<ParameterDescriptor<?>>();
+      foo.addAll(descriptor.getArguments());
+      foo.addAll(descriptor.getOptions());
+
+      //
+      List<Match.Parameter<?, ParameterBinding.ClassField>> invocation = new ArrayList<Match.Parameter<?, ParameterBinding.ClassField>>();
+      MatchIterator<T, ParameterBinding.ClassField> iterator = analyzer.analyzer(s);
+      while (iterator.hasNext()) {
+
+        Match<ParameterBinding.ClassField> match = iterator.next();
+        if (match instanceof Match.Parameter<?, ?>) {
+          Match.Parameter<?, ParameterBinding.ClassField> parameterMatch = (Match.Parameter<?, ParameterBinding.ClassField>)match;
+          if (!foo.remove(parameterMatch.getParameter())) {
+            throw new SyntaxException();
+          }
+          invocation.add(parameterMatch);
+        }
+      }
+
+      // Should be better with required / non required
+      if (!foo.isEmpty()) {
+        throw new SyntaxException();
+      }
+
+      //
+      for (Match.Parameter<?, ParameterBinding.ClassField> parameterMatch : invocation) {
+
+        ParameterDescriptor<ParameterBinding.ClassField> parameter = parameterMatch.getParameter();
+        ParameterBinding.ClassField cf = parameter.getBinding();
+        Field f = cf.getField();
+
+        //
+        Object v;
+        if (parameter.getType().getMultiplicity() == Multiplicity.LIST) {
+          v = parameterMatch.getValues();
+        } else {
+          v = parameterMatch.getValues().get(0);
+        }
+
+        //
+        f.setAccessible(true);
+        try {
+          f.set(object, v);
+        }
+        catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+      }
+    }
+  }
+}
---
+++ shell/core/src/test/java/org/crsh/cmdline/processor/CmdLineProcessorTestCase.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.crsh.cmdline.processor;
+
+import junit.framework.TestCase;
+import org.crsh.cmdline.CommandDescriptor;
+import org.crsh.cmdline.IntrospectionException;
+import org.crsh.cmdline.Option;
+import org.crsh.cmdline.ParameterBinding;
+import org.crsh.cmdline.processor.CmdLineProcessor;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+public class CmdLineProcessorTestCase extends TestCase {
+
+
+  public void testFoo() throws Exception {
+
+    class A {
+      @Option(opt = 'o')
+      String s;
+    }
+
+    CommandDescriptor<A, ParameterBinding.ClassField> desc = CommandDescriptor.create(A.class);
+    CmdLineProcessor.Clazz<A> conf = new CmdLineProcessor.Clazz<A>(desc);
+
+    A a = new A();
+    conf.process(a, "-o foo");
+    assertEquals("foo", a.s);
+
+    try {
+      conf.process(a, "");
+      fail();
+    }
+    catch (SyntaxException e) {
+    }
+  }
+
+
+}
