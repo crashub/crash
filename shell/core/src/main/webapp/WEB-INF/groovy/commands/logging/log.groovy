b68b9540f9bf06d4ee3f6b8f71319cbb0e71db28
julien.viet <julien.viet@118f38d8-d83b-11de-afbc-5bc42a3db48a>
2010-12-22 22:37:47 +0000

    start to concatenate log commands

--- shell/core/src/main/webapp/WEB-INF/groovy/commands/logging/log.groovy
+++ shell/core/src/main/webapp/WEB-INF/groovy/commands/logging/log.groovy
@@ -5,26 +5,24 @@ import org.crsh.command.Description;
 import org.crsh.command.CommandContext;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import java.util.logging.LogManager;
+import java.util.logging.LoggingMXBean;
+import java.util.Collections;
+import java.util.regex.Pattern;
+import javax.management.ObjectName;
 
-@Description("Log a message to a logger")
-public class log extends org.crsh.command.CRaSHCommand<Logger, Void> {
+public class log extends org.crsh.command.CRaSHCommand {
 
   /** The logger methods.*/
   private static final Set<String> methods = ["trace", "debug", "info", "warn", "trace"];
 
-  @Description("The logger name")
-  @Argument
-  def String name;
-
-  @Description("The logger level (default info)")
-  @Option(names=["l","level"])
-  def String levelName;
-
-  @Description("The message to log")
-  @Option(names=["m","message"],required=true)
-  def String msg;
-
-  public void execute(CommandContext<Logger, Void> context) throws ScriptException {
+  @Description("Log a message to a logger")
+  @org.crsh.cmdline.Command
+  public void main(
+    CommandContext<Logger, Void> context,
+    @Description("The message to log") @Option(names=["m","message"],required=true) String msg,
+    @Description("The logger name") @Argument String name,
+    @Description("The logger level (default info)") @Option(names=["l","level"]) String levelName) throws ScriptException {
 
     //
     if (levelName == null)
@@ -33,20 +31,77 @@ public class log extends org.crsh.command.CRaSHCommand<Logger, Void> {
     //
     if (context.piped) {
       context.consume().each() {
-        doLog(it);
+        doLog(it, levelName, msg);
       }
     } else {
       if (name != null) {
         def logger = LoggerFactory.getLogger(name);
-        doLog(logger);
+        doLog(logger, levelName, msg);
       }
     }
   }
 
-  private void doLog(Logger logger) {
+  private void doLog(Logger logger, String levelName, String msg) {
     String methodName = levelName.toLowerCase();
     if (!methods.contains(levelName))
       throw new ScriptException("Unrecognized log level $levelName");
     logger."$methodName"(msg);
+  }
+
+  @Description("List the available loggers")
+  @org.crsh.cmdline.Command
+  public void ls(
+    CommandContext<Void, Logger> context,
+    @Option(names=["f","filter"])
+    @Description("Filter the logger with a regular expression")
+    String filter) throws ScriptException {
+
+    // Regex filter
+    def pattern = Pattern.compile(filter != null ? filter : ".*");
+
+    //
+    def names = [] as Set;
+    def factory = LoggerFactory.ILoggerFactory;
+    def factoryName = factory.class.simpleName;
+    if (factoryName.equals("JDK14LoggerFactory")) {
+      // JDK
+      LogManager mgr = LogManager.logManager;
+      LoggingMXBean mbean = mgr.loggingMXBean;
+
+      // Add the known names
+      names.addAll(mbean.loggerNames);
+
+      // This is a trick to get the logger names per web application in Tomcat environment
+      def server = org.apache.tomcat.util.modeler.Registry.registry.MBeanServer;
+      ObjectName on = new ObjectName("*:j2eeType=WebModule,*");
+      def res = server.queryNames(on, null).each {
+        def loader = server.getAttribute(it, "loader");
+        def oldCL = Thread.currentThread().contextClassLoader;
+        try {
+          Thread.currentThread().contextClassLoader = loader.classLoader;
+          names.addAll(mbean.loggerNames);
+          } finally {
+          Thread.currentThread().contextClassLoader = oldCL;
+        }
+      }
+    } else if (factoryName.equals("JBossLoggerFactory")) {
+      // JBoss AS
+      def f = factory.class.getDeclaredField("loggerMap");
+      f.accessible = true;
+      def loggers = f.get(factory);
+      names.addAll(loggers.keySet());
+    } else {
+      System.out.println("Implement log lister for implementation " + factory.getClass().getName());
+    }
+
+    //
+    names.each {
+       def matcher = it =~ pattern;
+       if (matcher.matches()) {
+         def logger = LoggerFactory.getLogger(it);
+         context.produce(logger);
+         context.writer.println(it);
+       }
+    }
   }
 }
