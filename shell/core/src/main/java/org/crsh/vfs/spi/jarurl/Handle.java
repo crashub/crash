8db7f73d3efa411afffb38b098597a9b9ff18489
julien.viet <julien.viet@118f38d8-d83b-11de-afbc-5bc42a3db48a>
2010-12-31 01:23:04 +0000

    bar

---
+++ shell/core/src/main/java/org/crsh/vfs/spi/jarurl/Handle.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.crsh.vfs.spi.jarurl;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.jar.JarEntry;
+
+/**
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+public class Handle {
+
+  /** . */
+  private final JarURLDriver driver;
+
+  /** . */
+  final String name;
+
+  /** . */
+  Map<String, Handle> children;
+
+  /** . */
+  JarEntry entry;
+
+  Handle(JarURLDriver driver, String name) {
+    this.driver = driver;
+    this.name = name;
+    this.children = new HashMap<String, Handle>();
+  }
+
+  public boolean isDir() {
+     return entry == null || entry.isDirectory();
+  }
+
+  public URL toURL() throws IllegalArgumentException, IllegalStateException, MalformedURLException {
+     if (isDir()) {
+        throw new IllegalStateException("Cannot create dir URL");
+     }
+     return new URL("jar", "", driver.jarURL.toString() + "!/" + entry.getName());
+  }
+}
