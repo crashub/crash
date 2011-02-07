70ad06084d0ec0dba96010a9b15b32d4c087279b
julien.viet <julien.viet@118f38d8-d83b-11de-afbc-5bc42a3db48a>
2011-02-07 14:51:38 +0000

    converted setperm to use cmdline

--- jcr/src/main/resources/crash/commands/jcr/setperm.groovy
+++ jcr/src/main/resources/crash/commands/jcr/setperm.groovy
@@ -1,28 +1,19 @@
-import org.crsh.jcr.PropertyType;
 import org.crsh.command.ScriptException;
-import org.kohsuke.args4j.Argument;
-import org.kohsuke.args4j.Option;
 import javax.jcr.Node;
-import javax.jcr.Property;
-import org.crsh.command.Description;
 import org.exoplatform.services.jcr.access.AccessControlList;
-import java.util.ArrayList;
-import org.crsh.command.InvocationContext;
+import org.crsh.command.InvocationContext
+import org.crsh.cmdline.annotations.Command
+import org.crsh.cmdline.annotations.Usage
+import org.crsh.cmdline.annotations.Man
+import org.crsh.jcr.command.Path
+import org.crsh.cmdline.annotations.Option
+import org.crsh.cmdline.annotations.Argument;
 
-@Description("Set the permissions on a JCR node, the mixing exo:privilegeable is added when it is not already present")
-public class setperm extends org.crsh.command.BaseCommand<Node, Node> {
+public class setperm extends org.crsh.jcr.command.JCRCommand {
 
-  @Argument(index = 0,metaVar="paths",usage="The paths to alter security")
-  def List<String> paths;
-
-  @Option(name="-i",aliases=["--identity"],usage="The identity to use")
-  def String identity;
-
-  @Option(name="-a",aliases=["--add"],usage="The permission to add")
-  List<String> toAdd;
-
-  @Option(name="-r",aliases=["--remove"],usage="The permission to remove")
-  List<String> toRemove;
+  @Option(names = ["i","identity"]) @Usage("the identity") def String identity;
+  @Option(names = ["a","add"]) @Usage("the permissions to use") def List<String> toAdd;
+  @Option(names = ["r","remove"]) @Usage("the permissions to remove") def List<String> toRemove;
 
   /**
    * Set permission on a single node.
@@ -53,7 +44,36 @@ public class setperm extends org.crsh.command.BaseCommand<Node, Node> {
     node.setPermission(identity, permissions.toArray(new String[permissions.size()]));
   }
 
-  public void execute(InvocationContext<Node, Node> context) throws ScriptException {
+  @Command
+  @Usage("modify the security permissions of a JCR node")
+  @Man("""The setperm commands configures the security of a node based on (see eXo JCR access control at
+http://wiki.exoplatform.com/xwiki/bin/view/JCR/Access%20Control). When a node is protected by access control, it owns a
+mixin named exo:privilegeable that contains a exo:permissions property, for instance:
+
+[/production]% ls
+/production
++-properties
+| +-jcr:primaryType: nt:unstructured
+| +-jcr:mixinTypes: [exo:privilegeable]
+| +-exo:permissions: [*:/platform/administrators read,*:/platform/administrators add_node,*:/platform/administrators set_property,*:/platform/administrators remove]
++-children
+| +-/production/app:gadgets
+| +-/production/app:applications
+| +-/production/mop:workspace
+
+You can alter the node permission list with the setperm command:
+
+[/production]% setperm -i *:/platform/mygroup -a read -a add_node /
+Node /production updated to [read,add_node]
+
+You can also remove a permission by using the -r option.
+
+[/production]% setperm -i *:/platform/mygroup -r add_node /
+Node /production updated to [read]
+
+The setperm command will add automatically the exo:privilegeable mixin on the node when it is missing. The setperm is
+a <Node,Void> command altering the security of the consumed node stream.""")
+  public void main(InvocationContext<Node, Node> context, @Path @Argument @Usage("the nodes path to secure") List<String> paths) throws ScriptException {
 
     //
     context.writer <<= "Updates permissions of nodes";
