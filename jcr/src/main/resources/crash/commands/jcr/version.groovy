5fe46fc2b47495282492dcdc9258ad437dcd9e03
julien.viet <julien.viet@118f38d8-d83b-11de-afbc-5bc42a3db48a>
2011-02-07 00:22:45 +0000

    cosmetic

--- jcr/src/main/resources/crash/commands/jcr/version.groovy
+++ jcr/src/main/resources/crash/commands/jcr/version.groovy
@@ -3,12 +3,12 @@ import org.crsh.cmdline.annotations.Usage
 import org.crsh.cmdline.annotations.Command
 import org.crsh.cmdline.annotations.Argument
 
-@Usage("version checkin and checkout")
-public class check extends org.crsh.jcr.command.JCRCommand {
+@Usage("versioning commands")
+public class version extends org.crsh.jcr.command.JCRCommand {
   // The path of the node to checkin
   @Usage("Checkin a node")
   @Command
-  public void IN(@Path @Argument String path) throws ScriptException {
+  public void checkin(@Path @Argument String path) throws ScriptException {
     assertConnected();
     def node = findNodeByPath(path);
     node.checkin();
@@ -17,7 +17,7 @@ public class check extends org.crsh.jcr.command.JCRCommand {
   // The path of the node to checkout
   @Usage("Checkout a node")
   @Command
-  public void OUT(@Path @Argument String path) throws ScriptException {
+  public void checkout(@Path @Argument String path) throws ScriptException {
     assertConnected();
     def node = findNodeByPath(path);
     node.checkout();
