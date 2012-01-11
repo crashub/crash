a2bb9ed3c3f646b91425fe3ffa4ed181d5327456
Julien Viet <julien@julienviet.com>
2012-01-11 16:05:32 +0100

    merge the memory command in the system command

--- shell/core/src/main/resources/crash/commands/base/system.groovy
+++ shell/core/src/main/resources/crash/commands/base/system.groovy
@@ -11,6 +11,7 @@ import org.crsh.cmdline.annotations.Argument
 import org.crsh.cmdline.spi.Value
 import org.crsh.cmdline.spi.Completer
 import org.crsh.cmdline.ParameterDescriptor
+import org.crsh.cmdline.annotations.Option
 
 /**
  * @author <a href="mailto:alain.defrance@exoplatform.com">Alain Defrance</a>
@@ -19,9 +20,11 @@ import org.crsh.cmdline.ParameterDescriptor
 @Usage("vm system properties commands")
 class system extends CRaSHCommand implements Completer {
 
+  // Properties command
+
   @Usage("list the vm system properties")
   @Command
-  public void ls(
+  public void propls(
     InvocationContext<Void, Map.Entry> context) {
 
     def formatString = "%1\$-35s %2\$-20s\r\n";
@@ -36,26 +39,26 @@ class system extends CRaSHCommand implements Completer {
     }
   }
 
-  @Usage("set the vm system properties")
+  @Usage("set a system property")
   @Command
-  public void set(
+  public void propset(
     InvocationContext<Void, Void> context,
     @PropertyName PropName name,
     @PropertyValue String value) {
     System.setProperty name.toString(), value
   }
 
-  @Usage("get the vm system properties")
+  @Usage("get a system property")
   @Command
-  public void get(
+  public void propget(
     InvocationContext<Void, Void> context,
     @PropertyName PropName name) {
     context.writer.print System.getProperty(name.toString())
   }
 
-  @Usage("remove the vm system properties")
+  @Usage("remove a system property")
   @Command
-  public void remove(
+  public void proprm(
     InvocationContext<Void, Void> context,
     @PropertyName PropName name) {
     System.clearProperty name.toString()
@@ -72,6 +75,39 @@ class system extends CRaSHCommand implements Completer {
       }
     }
     return c;
+  }
+
+  // Memory commands
+
+  @Usage("call garbage collector")
+  @Command
+  public void gc(
+      InvocationContext<Void, Map.Entry> context) {
+    System.gc()
+  }
+
+  @Usage("show free memory")
+  @Command
+  public void freemem(
+      InvocationContext<Void, Map.Entry> context,
+      @UnitOpt Unit unit,
+      @DecimalOpt Integer decimal) {
+    if (unit == null) {
+      unit = Unit.B;
+    }
+    context.writer.println(unit.compute(Runtime.getRuntime().freeMemory(), decimal) + unit.human)
+  }
+
+  @Usage("show total memory")
+  @Command
+  public void totalmem(
+      InvocationContext<Void, Map.Entry> context,
+      @UnitOpt Unit unit,
+      @DecimalOpt Integer decimal) {
+    if (unit == null) {
+      unit = Unit.B;
+    }
+    context.writer.println(unit.compute(Runtime.getRuntime().totalMemory(), decimal) + unit.human)
   }
 }
 
@@ -91,3 +127,36 @@ class PropName extends Value {
 @Man("The value of the property")
 @Argument(name = "value")
 @interface PropertyValue { }
+
+enum Unit { B(1, "b"), K(1024, "Kb"), M(1024 * 1024, "Mb"), G(1024 * 1024 * 1024, "Gb")
+  final long unit;
+  final String human;
+
+  Unit(long unit, String human) {
+    this.unit = unit;
+    this.human = human;
+  }
+
+  public String compute(long space, Integer decimal) {
+    if (decimal == null) {
+      decimal = 0
+    }
+    return new BigDecimal(space / unit).setScale(decimal, BigDecimal.ROUND_HALF_UP).toPlainString();
+  }
+
+  public String getHuman() {
+    return this.human;
+  }
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Usage("the unit of the memory space size")
+@Man("The unit of the memory space size {(B)yte, (O)ctet, (M)egaOctet, (G)igaOctet}")
+@Option(names=["u","unit"],completer=org.crsh.cmdline.EnumCompleter)
+@interface UnitOpt { }
+
+@Retention(RetentionPolicy.RUNTIME)
+@Usage("number of decimal")
+@Man("The number of decimal (default 0)")
+@Option(names=["d","decimal"])
+@interface DecimalOpt { }
