b500026a0eb8c85097ede3a1fc984cc25bd45a95
Gomez Henri <henri.gomez@gmail.com>
2012-04-14 13:33:00 +0200

    bootstrap added for Ubuntu compat

--- shell/packaging/src/main/packaging/bin/crash.sh
+++ shell/packaging/src/main/packaging/bin/crash.sh
@@ -28,8 +28,12 @@ done
 mkdir -p $CRASH_HOME/tmp
 
 # Hotspot and OpenJDK requires tools.jar in CLASSPATH for VirtualMachine
-[ -n "$JAVA_HOME" ] && CLASSPATH=$JAVA_HOME/lib/tools.jar
+if [ -n "$JAVA_HOME" ]; then
+	TOOLS_JAR=$JAVA_HOME/lib/tools.jar
+	CLASSPATH=$TOOLS_JAR
+	BOOTCP=-Xbootclasspath/a:$TOOLS_JAR
+fi
 
-export CLASSPATH=$CLASSPATH:$CRASH_HOME/bin/crsh.shell.core-1.0.0-cr2-standalone.jar
+export CLASSPATH=$CLASSPATH:$CRASH_HOME/bin/crsh.shell.core-${project.version}-standalone.jar
 
-java -Djava.util.logging.config.file=$CRASH_HOME/conf/logging.properties org.crsh.standalone.CRaSH $EXT_JARS --cmd $CRASH_HOME/cmd --property crash.vfs.refresh_period=1 $@
+java $BOOTCP -Djava.util.logging.config.file=$CRASH_HOME/conf/logging.properties org.crsh.standalone.CRaSH $EXT_JARS --cmd $CRASH_HOME/cmd --property crash.vfs.refresh_period=1 $@
