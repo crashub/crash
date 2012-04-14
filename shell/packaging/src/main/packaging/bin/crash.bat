11d19b102ecc9dd4e5c39c55a865897ceb9faaa3
Julien Viet <julien@julienviet.com>
2012-04-14 11:09:28 +0200

    CRASH-74 : BAT startup script

---
+++ shell/packaging/src/main/packaging/bin/crash.bat
@@ -0,0 +1,43 @@
+@echo off
+REM init variables
+set CMD_LINE_ARGS=
+set EXT_JARS=
+
+REM set jar name
+set JARNAME=crsh.shell.core-${project.version}.jar
+
+REM Get standard environment variables
+set PRGDIR=%~dp0
+
+REM Only set CRASH_HOME if not already set
+if not ".%CRASH_HOME%" == "." goto setupArgs
+pushd 
+cd %~dp0\..
+set CRASH_HOME=%CD%
+popd
+
+:setupArgs
+REM copy args one by one from the command line
+if "%1%a"=="a" goto setClasspath
+set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
+shift
+goto setupArgs
+
+:setClasspath
+REM if JAVA_HOME is set, add tools.jar to classpath
+if ".%JAVA_HOME%" == "."  goto addCrashJar
+set CLASSPATH=%JAVA_HOME%\lib\tools.jar
+set TOOLS_JAR=%JAVA_HOME%\lib\tools.jar
+
+:addCrashJar
+set CLASSPATH=%CLASSPATH%;%CRASH_HOME%\bin\%JARNAME%
+
+:setJars
+REM add all jars from the lib directory to the classpath
+for %%F in (%CRASH_HOME%\lib\*.jar) do set CLASSPATH=%CLASSPATH%;%%F
+
+REM Create tmp dir if it does not exist
+mkdir  %CRASH_HOME%\tmp
+
+REM start the application with all parameters. Add tools.jar to the bootclasspath, otherwise it cannot be found
+java -Xbootclasspath/a:"%TOOLS_JAR%" -classpath "%CLASSPATH%" -Djava.util.logging.config.file="%CRASH_HOME%\conf\logging.properties" -jar "%CRASH_HOME%\bin\%JARNAME%" --cmd "%CRASH_HOME%\cmd" --property crash.vfs.refresh_period=1 %CMD_LINE_ARGS%
