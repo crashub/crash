87f072eb7367820113a17e0909ae22a7c7d53465
Julien Viet <julien@julienviet.com>
2012-04-29 23:40:17 +0200

    move dbscript to test as it should not be in the base distro

--- shell/core/src/test/resources/crash/commands/test/dbscript.groovy
+++ shell/core/src/test/resources/crash/commands/test/dbscript.groovy
@@ -1,3 +1,4 @@
+package crash.commands.test
 jdbc.connect "jdbc:derby:memory:EmbeddedDB;create=true"
 jdbc.execute "create table derbyDB(num int, addr varchar(40))"
 jdbc.execute "insert into derbyDB values (1956,'Webster St.')"
