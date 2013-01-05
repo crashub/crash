327bad26f7463792bf7597e9686ea7680b4bc9b0
David Dossot <david@dossot.net>
2013-01-04 18:34:29 -0800

    added app flows listing

--- shell/packaging/src/main/mule/classes/crash/commands/base/mule.groovy
+++ shell/packaging/src/main/mule/classes/crash/commands/base/mule.groovy
@@ -7,6 +7,7 @@ import org.crsh.text.ui.UIBuilder
 
 import javax.management.MBeanServerFactory
 import javax.management.MBeanServer
+import javax.management.ObjectName
 
 @Usage("mule commands")
 class mule extends CRaSHCommand {
@@ -32,7 +33,7 @@ class mule extends CRaSHCommand {
   @Usage("list the names of all deployed applications")
   @Command
   void apps() {
-      mbeanServer.domains.each { domain ->
+      mbeanServer.domains.sort().each { domain ->
           if (isMuleApplicationDomain(domain)) {
               def muleContextMBean = new GroovyMBean(mbeanServer, "$domain:name=MuleContext")
               context.provide([name:domain.substring(5),'start time':muleContextMBean.StartTime])
@@ -40,22 +41,37 @@ class mule extends CRaSHCommand {
       }
   }
 
-  @Usage("print an application's statistics")
+  @Usage("print the statistics for an application or a flow within an application")
   @Command
-  void stats(@Usage("The application name") @Required @Argument String applicationName) {
-      def statisticsMBean = new GroovyMBean(mbeanServer, "Mule.$applicationName:type=org.mule.Statistics,Application=application totals")
-      if (statisticsMBean == null) {
-          out << "No application named $applicationName has been found\n"
-      } else {
+  void stats(@Usage("The application name") @Required @Option(names=["a","app"]) String applicationName,
+             @Usage("The flow name") @Option(names=["f","flow"]) String flowName) {
+
+      try {
+          def statisticsMBeanObjectName =
+              "Mule.$applicationName:type=org.mule.Statistics,"
+              .plus(flowName != null ? "Flow=$flowName" : 'Application=application totals')
+
+          def statisticsMBean = new GroovyMBean(mbeanServer, statisticsMBeanObjectName)
           statisticsMBean.listAttributeNames().each { statisticName ->
               context.provide([name:statisticName,value:statisticsMBean[statisticName]])
           }
+      } catch (Exception e) {
+          out << "Failed to locate statistics for the provided parameters ($e.message)\n"
       }
   }
 
+  @Usage("list the names of all the flows of an application")
+  @Command
+  void flows(@Usage("The application name") @Required @Option(names=["a","app"]) String applicationName) {
+      def flowMBeanPattern = new ObjectName("Mule.$applicationName:type=Flow,name=\"*\"")
+      mbeanServer.queryNames(flowMBeanPattern, null).sort().each { objectName ->
+          def flowMBean = new GroovyMBean(mbeanServer, objectName)
+          context.provide([name:flowMBean.Name])
+      }
+  }
+
+  // TODO list connectors and endpoints in app
   // TODO control one app
-  // TODO list flows, connectors and endpoints in app
-  // TODO get one flow stats
   // TODO control one flow, connector or endpoint
 
   private GroovyMBean getFirstMuleContextMBean() {
