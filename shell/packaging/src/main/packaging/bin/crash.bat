4da6d63790ad6e99afe4f0a4d8ad3d44bed91735
Damien Rieu <damien.rieu@gmail.com>
2013-04-03 15:59:44 +0200

    Fix bug CRASH 113

--- shell/packaging/src/main/packaging/bin/crash.bat
+++ shell/packaging/src/main/packaging/bin/crash.bat
@@ -1,4 +1,6 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
+
 REM init variables
 set CMD_LINE_ARGS=
 
@@ -7,17 +9,13 @@ set PRGDIR=%~dp0
 
 REM Only set CRASH_HOME if not already set
 if not ".%CRASH_HOME%" == "." goto setupArgs
-pushd 
-cd %~dp0\..
+pushd %PRGDIR%..
 set CRASH_HOME=%CD%
 popd
 
 :setupArgs
-REM copy args one by one from the command line
-if "%1%a"=="a" goto setClasspath
-set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
-shift
-goto setupArgs
+REM copy args from the command line
+set CMD_LINE_ARGS=%*
 
 :setClasspath
 REM if JAVA_HOME is set, add tools.jar to classpath
@@ -25,29 +23,22 @@ if ".%JAVA_HOME%" == "."  goto addCrashJar
 set CLASSPATH=%JAVA_HOME%\lib\tools.jar
 set TOOLS_JAR=%JAVA_HOME%\lib\tools.jar
 
-
 :addCrashJar
 for %%F in (%CRASH_HOME%\bin\crsh.cli*.jar) do set JARNAME=%%~nxF
 set CLASSPATH=%CLASSPATH%;%CRASH_HOME%\bin\%JARNAME%
 
 :setJars
 REM add all jars from the lib directory to the classpath
-for %%F in (%CRASH_HOME%\lib\*.jar) do (
-	call :concat %%F
-)
+for %%F in (%CRASH_HOME%\lib\*.jar) do set CLASSPATH=!CLASSPATH!;%%F
 
 REM Create tmp dir if it does not exist
-mkdir  %CRASH_HOME%\tmp
-
-set CLASSPATH=%CLASSPATH%;%LIB%
+if not exist "%CRASH_HOME%\tmp" mkdir %CRASH_HOME%\tmp
 
 REM start the application with all parameters. Add tools.jar to the bootclasspath, otherwise it cannot be found
-java -Xbootclasspath/a:"%TOOLS_JAR%" -classpath "%CLASSPATH%" -Djava.util.logging.config.file=%CRASH_HOME%/conf/logging.properties org.crsh.cli.impl.bootstrap.Main -jar "%CRASH_HOME%\bin\%JARNAME%" --conf "%CRASH_HOME%\conf" --cmd "%CRASH_HOME%\cmd" %CMD_LINE_ARGS%
+REM echo "java -Xbootclasspath/a:%TOOLS_JAR% -classpath %CLASSPATH% %MAVEN_DEBUG_OPTS% -Djava.util.logging.config.file=%CRASH_HOME%\conf\logging.properties org.crsh.cli.impl.bootstrap.Main --conf %CRASH_HOME%\conf --cmd %CRASH_HOME%\cmd %CMD_LINE_ARGS%"
 
-:concat
-if "%LIB%" == "" (
-	set LIB=%1
-) else (
-	set LIB=%LIB%;%1;
-)
-goto :eof
+java -Xbootclasspath/a:"%TOOLS_JAR%" -classpath "%CLASSPATH%" %MAVEN_DEBUG_OPTS% -Djava.util.logging.config.file="%CRASH_HOME%\conf\logging.properties" org.crsh.cli.impl.bootstrap.Main -jar "%CRASH_HOME%\bin\%JARNAME%" --conf "%CRASH_HOME%\conf" --cmd "%CRASH_HOME%\cmd" %CMD_LINE_ARGS%
+
+set ERROR_CODE=%ERRORLEVEL%
+endlocal & set ERROR_CODE=%ERROR_CODE%
+exit /B %ERROR_CODE%
